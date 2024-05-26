5bab7f5a6d26d6f1a377dc0d1f6b1a248245ffac
OlegMuralev <92426862+OlegMuralev@users.noreply.github.com>
2024-05-26 20:22:15 +0500

    Workshop DONE

--- src/main/java/kz/ableacademy/WorkShopTest.java
+++ src/main/java/kz/ableacademy/WorkShopTest.java
@@ -8,6 +8,13 @@ import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class WorkShopTest {
+    private final SelenideElement firstname = $("#firstname");
+    private final SelenideElement lastname = $("#lastname");
+    private final SelenideElement email = $("#email_address");
+    private final SelenideElement password = $("#password");
+    private final SelenideElement confirmPassword = $("#password-confirmation");
+    private final SelenideElement notificationSuccess = $x("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div");
+    private final SelenideElement submitButton = $x("//*[@id=\"form-validate\"]/div/div[1]/button/span");
 
     @BeforeAll
     public static void setUp() {
@@ -15,10 +22,36 @@ public class WorkShopTest {
     }
 
     @Test
-    public void testCreateAccountWithValidData() {
+    public void testFormValidData() {
         open("https://magento.softwaretestingboard.com/customer/account/create/");
-        // дописать код здесь
+        firstname.setValue("Micle");
+        lastname.setValue("Jackson");
+        email.setValue(RandomStringGenerator.generateRandomString() + "@mail.com");
+        password.setValue("Abs12345678!");
+        confirmPassword.setValue("Abs12345678!");
+        submitButton.click();
+
+        // ожидаемый результат
+        notificationSuccess.shouldHave(Condition.text("Thank you for registering with Main Website Store."));
     }
 
+    @Test
+    public void testPasswordSevenSymbols(){
+        open("https://magento.softwaretestingboard.com/customer/account/create/");
+        password.setValue("Abs1234");
+        // ожидаемый результат
+        $("#password-error").shouldHave(Condition.text("Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored."));
+    }
+/*
+Тест-кейс 2 - Заполнить форму паролем 7 символов
+Шаги:
+1) Перейти на страницу https://magento.softwaretestingboard.com/customer/account/create/
+2) Заполнить поле Password - Abs1234
+Ожидаемый результат:
+Появилось сообщение - "Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored."
 
+| Элемент                    | Локатор                                         |
+| -------------------------- | ----------------------------------------------- |
+| Сообщение об ошибке        | #password-error                                      |
+*/
 }
